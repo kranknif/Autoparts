0390c7be222cab4bac187daf3641017c8f2a4774
Kabeus <kranknif@gmail.com>
2014-04-24 14:25:13 +0400

    Added db file autoparts.sql

---
+++ httpdocs/protected/data/autoparts.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2014 at 12:13 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `autoparts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `autopart`
+--
+
+CREATE TABLE IF NOT EXISTS `autopart` (
+  `autopart_id` int(11) NOT NULL AUTO_INCREMENT,
+  `parttype_id` int(11) NOT NULL,
+  `car_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `producer_id` int(11) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`autopart_id`),
+  KEY `parttype_id` (`parttype_id`),
+  KEY `producer_id` (`producer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `autopart`
+--
+
+INSERT INTO `autopart` (`autopart_id`, `parttype_id`, `car_id`, `name`, `quantity`, `producer_id`, `description`, `price`) VALUES
+(1, 1, 1, 'TestName', 122, 1, 'TestDescription', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car`
+--
+
+CREATE TABLE IF NOT EXISTS `car` (
+  `car_id` int(11) NOT NULL AUTO_INCREMENT,
+  `producer` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  PRIMARY KEY (`car_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `car`
+--
+
+INSERT INTO `car` (`car_id`, `producer`, `model`) VALUES
+(1, 'Mercedes', '190E Cosworth');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parttype`
+--
+
+CREATE TABLE IF NOT EXISTS `parttype` (
+  `parttype_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`parttype_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `producer`
+--
+
+CREATE TABLE IF NOT EXISTS `producer` (
+  `producer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  PRIMARY KEY (`producer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
