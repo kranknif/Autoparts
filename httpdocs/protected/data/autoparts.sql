e3d04fcd46f0feb811cf2b1224e462c78123761b
Kabeus <kranknif@gmail.com>
2014-05-20 16:15:13 +0400

    db update

--- httpdocs/protected/data/autoparts.sql
+++ httpdocs/protected/data/autoparts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2014 at 12:05 PM
+-- Generation Time: May 20, 2014 at 02:05 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -38,15 +38,17 @@ CREATE TABLE IF NOT EXISTS `autopart` (
   PRIMARY KEY (`autopart_id`),
   KEY `parttype_id` (`parttype_id`),
   KEY `producer_id` (`producer_id`),
-  KEY `car_id` (`car_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  KEY `car_id` (`car_id`),
+  KEY `producer_id_2` (`producer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `autopart`
 --
 
 INSERT INTO `autopart` (`autopart_id`, `parttype_id`, `car_id`, `name`, `quantity`, `producer_id`, `description`, `price`) VALUES
-(3, 1, 1, 'Колесо ', 2, 1, '', 11);
+(3, 1, 2, 'Дверь правая', 1, 3, 'Дверь правая с ручкой', 3),
+(4, 3, 2, 'багажник', 122, 3, 'багажник багажник', 14);
 
 -- --------------------------------------------------------
 
@@ -59,14 +61,15 @@ CREATE TABLE IF NOT EXISTS `car` (
   `producer` varchar(50) NOT NULL,
   `model` varchar(50) NOT NULL,
   PRIMARY KEY (`car_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `car`
 --
 
 INSERT INTO `car` (`car_id`, `producer`, `model`) VALUES
-(1, 'Mercedes', '190E Cosworth');
+(2, '', '428i Cabriolet'),
+(3, '', 'A3 Sedan');
 
 -- --------------------------------------------------------
 
@@ -78,14 +81,16 @@ CREATE TABLE IF NOT EXISTS `parttype` (
   `parttype_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`parttype_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `parttype`
 --
 
 INSERT INTO `parttype` (`parttype_id`, `name`) VALUES
-(1, 'Дверь');
+(1, 'Дверь'),
+(2, 'Лобовое стекло'),
+(3, 'Багажник');
 
 -- --------------------------------------------------------
 
@@ -98,14 +103,17 @@ CREATE TABLE IF NOT EXISTS `producer` (
   `name` varchar(50) NOT NULL,
   `phone` varchar(50) NOT NULL,
   PRIMARY KEY (`producer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `producer`
 --
 
 INSERT INTO `producer` (`producer_id`, `name`, `phone`) VALUES
-(1, 'Бразерс', '89995558866');
+(1, 'Бразерс', '89995558866'),
+(2, 'Audi', '827258645'),
+(3, 'BMW', '49756674956'),
+(4, 'Mercedes', '49888767534');
 
 --
 -- Constraints for dumped tables
@@ -115,9 +123,9 @@ INSERT INTO `producer` (`producer_id`, `name`, `phone`) VALUES
 -- Constraints for table `autopart`
 --
 ALTER TABLE `autopart`
-  ADD CONSTRAINT `autopart_ibfk_3` FOREIGN KEY (`car_id`) REFERENCES `car` (`car_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `autopart_ibfk_1` FOREIGN KEY (`parttype_id`) REFERENCES `parttype` (`parttype_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `autopart_ibfk_2` FOREIGN KEY (`producer_id`) REFERENCES `producer` (`producer_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `autopart_ibfk_2` FOREIGN KEY (`producer_id`) REFERENCES `producer` (`producer_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `autopart_ibfk_3` FOREIGN KEY (`car_id`) REFERENCES `car` (`car_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
